64cdc9a23ed742ddaa3c58abb844321d3e3b2b05
Asimeet Padhee <asimeet@asimeets-mbp.kpn>
2021-09-07 20:35:24 +0200

    config fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    PDP_BASEURL: process.env.NODE_ENV === 'development' ? 'http://localhost:5001': process.env.REACT_APP_PDP_BASEURL,
+    PDP_BASEURL: process.env.NODE_ENV === 'development' ? 'http://localhost:5001/api': `${process.env.REACT_APP_PDP_BASEURL}/api`,
     PDP_API_KEY: process.env.NODE_ENV === 'development' ? 'ext-ecom-002': process.env.REACT_APP_PDP_API_KEY
 }
 
